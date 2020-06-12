cef73a36d97ee221c8fe6742ffc7d5f038664d8c
Ron Norasak <ronnorasak@gmail.com>
2020-06-11 21:20:07 -0500

    more tweaks to integrate nav

--- App.js
+++ App.js
@@ -1,81 +1,57 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// // Firebase App (the core Firebase SDK) is always required and
-// // must be listed before other Firebase SDKs
-// import * as firebase from 'firebase/app';
-
-// // Add the Firebase services that you want to use
-// import 'firebase/auth';
-// import 'firebase/functions';
-// import 'firebase/database';
-
-// import { useObjectVal } from 'react-firebase-hooks/database';
-
-// // TODO: Replace the following with your app's Firebase project configuration
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDzCCPQjPzALATf_YOCXSpporWLGPX6OgA',
-//   authDomain: 'money-to-burn.firebaseapp.com',
-//   databaseURL: 'https://money-to-burn.firebaseio.com',
-//   projectId: 'money-to-burn',
-//   storageBucket: 'money-to-burn.appspot.com',
-//   messagingSenderId: '259957476230',
-//   appId: '1:259957476230:web:f7e68b8436ffd6bcbbba07',
-//   measurementId: 'G-2JNBJLSCLD',
-// };
-
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
-// var ref = firebase.database().ref();
-
-// export default function App() {
-//   const [value, loading, error] = useObjectVal(ref);
-//   console.log('value', value);
-//   return (
-//     <View style={styles.container}>
-//       {loading && <Text>Loading</Text>}
-//       {value && <Text>{JSON.stringify(value)}</Text>}
-//       {/* for not existing */}
-//       {value === null && <Text>DNE</Text>}
-//       {error && <Text>error</Text>}
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-// In App.js in a new project
-
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import Home from './screens/home';
+import Login from './screens/login';
 
 const Stack = createStackNavigator();
 
-function App() {
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from 'firebase/app';
+
+// Add the Firebase services that you want to use
+import 'firebase/auth';
+import 'firebase/functions';
+import 'firebase/database';
+
+import { useObjectVal } from 'react-firebase-hooks/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDfYmbv82EH6KCzzwOhT3DK_7ufoXfWbXU',
+  authDomain: 'money-to-burn---ron-db.firebaseapp.com',
+  databaseURL: 'https://money-to-burn---ron-db.firebaseio.com',
+  projectId: 'money-to-burn---ron-db',
+  storageBucket: 'money-to-burn---ron-db.appspot.com',
+  messagingSenderId: '508239108257',
+  appId: '1:508239108257:web:13cf7398e1fe29a26cb8e0',
+  measurementId: 'G-V8FFSPWNVY',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var ref = firebase.database().ref();
+
+export default function App() {
+  const [value, loading, error] = useObjectVal(ref);
+  const [count, setCount] = useState(0);
+  console.log('db', value);
+
+  async function buttonPress() {
+    await setCount(count + 1);
+    //update database
+    var countfire = firebase.database().ref();
+    countfire.update({ thisisavalue: count });
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
