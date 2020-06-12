d4ccdf898d92792018988b9b541e5389f4feaec6
Ron Norasak <ronnorasak@gmail.com>
2020-06-11 23:24:00 -0500

    got update of individual items

--- App.js
+++ App.js
@@ -34,10 +34,35 @@ firebase.initializeApp(firebaseConfig);
 
 var ref = firebase.database().ref();
 
+export const db = firebase.database();
+
+firebase
+  .auth()
+  .signInAnonymously()
+  .catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    console.log('user', uid);
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+  // ...
+});
+
 export default function App() {
   const [value, loading, error] = useObjectVal(ref);
   const [count, setCount] = useState(0);
-  console.log('db', value);
 
   async function buttonPress() {
     await setCount(count + 1);
--- screens/home.js
+++ screens/home.js
@@ -1,7 +1,17 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
+import { db } from '../App';
 
 export default function HomeScreen({ navigation }) {
+  const [count, setCount] = useState(0);
+  // console.log('db', db.ref('/GamesList/clikBait'));
+
+  function buttonPress() {
+    setCount(count + 1);
+    //update database
+    db.ref('/GamesList/clikBait/').update({ 1: count });
+  }
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
@@ -9,6 +19,7 @@ export default function HomeScreen({ navigation }) {
         title="Go to Login"
         onPress={() => navigation.navigate('Login')}
       />
+      <Button onPress={buttonPress} title="push me" color="red" />
     </View>
   );
 }
