f6e1b2ba27464ac585d4f618388508d3836ea0ea
eric wehrli <ejwehrli@gmail.com>
2020-06-21 22:45:32 -0500

    config update

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { ThemeProvider } from 'react-native-elements';
+import { ThemeProvider, Button } from 'react-native-elements';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { db } from './firebaseConfig';
 
@@ -14,13 +14,26 @@ import ClikBait from './screens/clikBait';
 
 const Stack = createStackNavigator();
 
+
+const theme = {
+  Button: {
+    titleStyle: {
+      color: 'red',
+    },
+    raised: true,
+  },
+  Text: {
+    fontSize: 30,
+  }
+};
+
 export default function App() {
   const [user, loading, error] = useAuthState(db.auth());
 
   console.log('home login id', user && user.uid);
 
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Home} />
