326e7501a1f822d192c983840e033b56c9b58745
Ron Norasak <ronnorasak@gmail.com>
2020-06-23 18:15:27 -0500

    small update

--- screens/asteroidAdventureUtilities/_setupCollisonHandler.js
+++ screens/asteroidAdventureUtilities/_setupCollisonHandler.js
@@ -25,7 +25,7 @@ const _setupCollisionHandler = (engine, width, roomName, uid) => {
     }
 
     if (objA === 'ball' && objB === 'debris') {
-      Alert.alert('Game Over', 'You lose...');
+      Alert.alert('Game Over');
       db.database()
         .ref(`/Rooms/${roomName}/Game/AliveStatus/`)
         .update({ [uid]: false });
