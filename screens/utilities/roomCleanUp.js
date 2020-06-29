4767ea53e1ec642ad7ba8c98a2e11032746534a0
Zach Beyel <zjabeyel@gmail.com>
2020-06-29 10:08:52 -0500

    added logic to room close to take stacks from better, need to add payout for winner

--- screens/utilities/roomCleanUp.js
+++ screens/utilities/roomCleanUp.js
@@ -6,6 +6,7 @@ import {
   useList,
   useObject,
 } from 'react-firebase-hooks/database';
+import { stopClock } from 'react-native-reanimated';
 
 //function will have two parts; one is to remove single user from playerList, the other is to delete the room if the player is the last player
 
@@ -15,9 +16,39 @@ export default function roomCleanUp(
   uid,
   playerReference
 ) {
+  async function betResolution(roomName, uid) {
+    const userDbRef = db.database().ref(`/Users/${uid}/`);
+    let userVals, stacksLost;
+    let bettingRoom;
+
+    try {
+      await db
+        .database()
+        .ref(`/Rooms/${roomName}/Bets`)
+        .once('value', (e) => console.log(e.val()))
+        .then((ret) => (bettingRoom = ret.val()));
+      await db
+        .database()
+        .ref(`/Users/${uid}/`)
+        .once('value', (user) => console.log(user))
+        .then((ret) => (userVals = ret.val()));
+
+      stacksLost = userVals.stacks + bettingRoom[uid];
+    } catch (error) {
+      console.log(error);
+    }
+
+    // console.log(stacksLost);
+    // console.log('bettingroom', bettingRoom);
+    // console.log('userVals', userVals);
+
+    // console.log(bettingRoom.val()[uid]);
+    userDbRef.update({ stacks: bettingRoom[uid] });
+  }
+
   let playerList = db.database().ref(`/Rooms/${roomName}/playerList/`);
   let rooms = db.database().ref(`/Rooms/`);
-
+  betResolution(roomName, uid);
   playerList.update({
     [uid]: null,
   });
